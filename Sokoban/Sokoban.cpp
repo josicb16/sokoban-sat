c5bf93ca3e3d5704b8891c06b1d7782f6cc2cb83
josicb16 <95657159+josicb16@users.noreply.github.com>
2022-05-20 12:27:58 +0200

    Dodata klasa za stanje problema

--- Sokoban/Sokoban.cpp
+++ Sokoban/Sokoban.cpp
@@ -1,22 +1,112 @@
 #include "Sokoban.hpp"
 
-#include <vector>
-#include <string>
-
 #include <iostream>
+#include <string>
+#include <vector>
 
-Sokoban::Sokoban() {
-	std::vector<std::string> _table;
-	table = _table;
+
+
+std::vector<bool> State::GetPlayerCoordinates() const {
+	return player_coordinates;
 }
+
+
+std::vector<bool> State::GetBoxesCoordinates() const {
+	return boxes_coordinates;
+}
+
+
+Sokoban::Sokoban(int _plan_length, std::vector<std::string> table) {
+	plan_length = _plan_length;
+	
+	int i, j;
+	n = table.size();
+	m = table[0].length();
+	
+	std::vector<bool> player_coordinates(n*m, false);
+	std::vector<bool> boxes_coordinates(n*m, false);
+	
+	std::vector<bool> tmp1(n*m, false);
+	walls = tmp1;
+	
+	std::vector<bool> tmp2(n*m, false);
+	box_home = tmp2;
+	
+	for(i=0; i<n; i++) {
+		for(j=0; j<m; j++) {
+			switch(table[i][j]) {
+				case '#':  // wall
+					walls[j*n+i] = true;
+					break;
+				case 'o':  // box_home
+					box_home[j*n+i] = true;
+					break;
+				case 's':  // Sokoban
+					player_coordinates[j*n+i] = true;
+					break;
+				case 'S':  // Sokoban; box_home
+					player_coordinates[j*n+i] = true;
+					box_home[j*n+i] = true;
+					break;
+				case 'b':  // box
+					boxes_coordinates[j*n+i] = true;
+					break;
+				case 'B':  // box; box_home
+					boxes_coordinates[j*n+i] = true;
+					box_home[j*n+i] = true;
+					break;
+				default: // prazan prostor
+					continue;
+			}
+		}
+	}
+	
+	state = new State(player_coordinates, boxes_coordinates);
+	
+}
+
+
+Sokoban::~Sokoban() {
+	delete state;
+}
+
 
 void Sokoban::PrintTable() const {
-	auto it = table.cbegin();
-	auto it_end = table.cend();
-
-	while(it != it_end) {
-		std::cout << *it << std::endl;
-		it++;
+	
+	std::vector<bool> player_coordinates = state->GetPlayerCoordinates();
+	std::vector<bool> boxes_coordinates = state->GetBoxesCoordinates();
+	
+	int i, j;
+	for(i=0; i<n; i++) {
+		for(j=0; j<m; j++) {
+			if(walls[j*n+i]) {
+				std::cout << "#";
+				continue;
+			}
+			else if(box_home[j*n+i]) {
+				if(boxes_coordinates[j*n+i]) 
+					std::cout << "B";
+				else if(player_coordinates[j*n+i])
+					std::cout << "S";
+				else
+					std::cout << "o";
+				continue;
+			}
+			else if(player_coordinates[j*n+i]) {
+				std::cout << "s";
+				continue;
+			}
+			else if(boxes_coordinates[j*n+i]) {
+				std::cout << "b";
+				continue;
+			}
+			
+			std::cout << " ";
+			
+		}
+		std::cout << std::endl;
 	}
+	
+	std::cout << std::endl;
+	
 }
-
--- Sokoban/Sokoban.hpp
+++ Sokoban/Sokoban.hpp
@@ -4,16 +4,39 @@
 #include <vector>
 #include <string>
 
+class State {
+	public:
+		State(std::vector<bool> _player_coordinates, std::vector<bool> _boxes_coordinates) 
+			: player_coordinates(_player_coordinates), boxes_coordinates(_boxes_coordinates) { 		}
+
+		std::vector<bool> GetPlayerCoordinates() const; 
+		std::vector<bool> GetBoxesCoordinates() const;
+
+	private:
+		std::vector<bool> player_coordinates;
+		std::vector<bool> boxes_coordinates;
+};
+
+
 
 class Sokoban {
 	public:
-		Sokoban();
-		Sokoban(std::vector<std::string> _table) : table(_table) {    }
+		Sokoban(int _plan_length, std::vector<std::string> table);
+		~Sokoban();
 
 		void PrintTable() const;
 
+
 	private:
-		std::vector<std::string> table;
+		int plan_length;
+		int n;  // napraviti klasu Table
+		int m;
+		std::vector<bool> walls;
+		std::vector<bool> box_home;
+		State *state;
 };
+
+
+
 
 #endif
