b55ab57dd87b2cd82112adeb9ea012e71b8f4a4b
josicb16 <95657159+josicb16@users.noreply.github.com>
2022-05-20 15:24:26 +0200

    Dodati metodi za nalaženje NNF formule

--- Sokoban/Formula.cpp
+++ Sokoban/Formula.cpp
@@ -2,78 +2,161 @@
 #include "Formula.hpp"
 
 #include <iostream>
+#include <memory>
+#include <string>
+#include <cstdlib>
 
-void Atom::printFormula() const {
+std::string True::GetType() const { return "true"; }
+std::string False::GetType() const { return "false"; }
+std::string Atom::GetType() const { return "atom"; }
+std::string Not::GetType() const { return "not"; }
+std::string And::GetType() const { return "and"; }
+std::string Or::GetType() const { return "or"; }
+std::string Impl::GetType() const { return "impl"; }
+std::string Eql::GetType() const { return "eql"; }
+
+Formula Not::GetFormulaData() const { return f; }
+
+Formula And::GetFormulaDataL() const { return l; }
+Formula Or::GetFormulaDataL() const { return l; }
+Formula Impl::GetFormulaDataL() const { return l; }
+Formula Eql::GetFormulaDataL() const { return l; }
+
+Formula And::GetFormulaDataR() const { return r; }
+Formula Or::GetFormulaDataR() const { return r; }
+Formula Impl::GetFormulaDataR() const { return r; }
+Formula Eql::GetFormulaDataR() const { return r; }
+
+
+
+void True::PrintFormula() const {
+	std::cout << "T";
+}
+void False::PrintFormula() const {
+	std::cout << "F";
+}
+void Atom::PrintFormula() const {
 	std::cout << n;
 }
-
-void Not::printFormula() const {
+void Not::PrintFormula() const {
 	std::cout << "~(";
-	f->printFormula();
+	f->PrintFormula();
 	std::cout << ")";
 }
-
-void And::printFormula() const {
+void And::PrintFormula() const {
 	std::cout << "(";
-	l->printFormula();
+	l->PrintFormula();
 	std::cout << ")";
 	std::cout << " && ";
 	std::cout << "(";
-	r->printFormula();
+	r->PrintFormula();
 	std::cout << ")";
 }
-
-void Or::printFormula() const {
+void Or::PrintFormula() const {
 	std::cout << "(";
-	l->printFormula();
+	l->PrintFormula();
 	std::cout << ")";
 	std::cout << " || ";
 	std::cout << "(";
-	r->printFormula();
+	r->PrintFormula();
 	std::cout << ")";
 }
-
-void Impl::printFormula() const {
+void Impl::PrintFormula() const {
 	std::cout << "(";
-	l->printFormula();
+	l->PrintFormula();
 	std::cout << ")";
 	std::cout << " => ";
 	std::cout << "(";
-	r->printFormula();
+	r->PrintFormula();
 	std::cout << ")";
 }
-
-void Eql::printFormula() const {
+void Eql::PrintFormula() const {
 	std::cout << "(";
-	l->printFormula();
+	l->PrintFormula();
 	std::cout << ")";
 	std::cout << " <=> ";
 	std::cout << "(";
-	r->printFormula();
+	r->PrintFormula();
 	std::cout << ")";
 }
 
-Not::~Not() {
-	delete f;
+
+
+Formula True::ToNNF() const {
+	return std::make_shared<True>();
+}
+Formula False::ToNNF() const {
+	return std::make_shared<False>();
+}
+Formula Atom::ToNNF() const {
+	return std::make_shared<Atom>(n);
+}
+Formula Not::ToNNF() const {
+	std::string type = f->GetType();
+	if(type=="true")
+		return std::make_shared<False>();
+	else if(type=="false")
+		return std::make_shared<True>();
+	else if(type=="atom")
+		return std::make_shared<Not>(f);
+	else if(type=="not")
+		return f->GetFormulaData();
+	else if(type=="and") {
+		Formula l = f->GetFormulaDataL();
+		Formula r = f->GetFormulaDataR();
+		Formula l1 = std::make_shared<Not>(l);
+		Formula r1 = std::make_shared<Not>(r);
+		Formula l1_tmp = l1->ToNNF();
+		Formula r1_tmp = r1->ToNNF();
+		return std::make_shared<Or>(l1_tmp, r1_tmp);
+	}
+	else if(type=="or") {
+		Formula l = f->GetFormulaDataL();
+		Formula r = f->GetFormulaDataR();
+		Formula l1 = std::make_shared<Not>(l);
+		Formula r1 = std::make_shared<Not>(r);
+		Formula l1_tmp = l1->ToNNF();
+		Formula r1_tmp = r1->ToNNF();
+		return std::make_shared<And>(l1_tmp, r1_tmp);
+	}
+	else if(type=="impl") {
+		Formula l = f->GetFormulaDataL();
+		Formula r = f->GetFormulaDataR();
+		Formula l1_tmp = l->ToNNF();
+		Formula r1 = std::make_shared<Not>(r);
+		Formula r1_tmp = r1->ToNNF();
+		return std::make_shared<And>(l1_tmp, r1_tmp);
+	}
+	else if(type=="eql") {
+		Formula l = f->GetFormulaDataL();
+		Formula r = f->GetFormulaDataR();
+		Formula l1 = std::make_shared<Impl>(l, r);
+		Formula r1 = std::make_shared<Impl>(r, l);
+		Formula tmp = std::make_shared<And>(l1, r1);
+		return tmp->ToNNF();
+	}
+}
+Formula And::ToNNF() const {
+	Formula l1 = l->ToNNF();
+	Formula r1 = r->ToNNF();
+	return std::make_shared<And>(l1, r1);
+}
+Formula Or::ToNNF() const {
+	Formula l1 = l->ToNNF();
+	Formula r1 = r->ToNNF();
+	return std::make_shared<Or>(l1, r1);
+}
+Formula Impl::ToNNF() const {
+	Formula l1 = std::make_shared<Not>(l);
+	Formula l1_tmp = l1->ToNNF();
+	Formula r1_tmp = r->ToNNF();
+	return std::make_shared<Or>(l1_tmp, r1_tmp);
+}
+Formula Eql::ToNNF() const {
+	Formula l1 = std::make_shared<Impl>(l, r);
+	Formula r1 = std::make_shared<Impl>(r, l);
+	Formula tmp = std::make_shared<And>(l1, r1);
+	return tmp->ToNNF();
 }
 
-And::~And() {
-	delete l;
-	delete r;
-}
-
-Or::~Or() {
-	delete l;
-	delete r;
-}
-
-Impl::~Impl() {
-	delete l;
-	delete r;
-}
-
-Eql::~Eql() {
-	delete l;
-	delete r;
-}
 
--- Sokoban/Formula.hpp
+++ Sokoban/Formula.hpp
@@ -1,33 +1,69 @@
 #ifndef __FORMULA_HPP__
 #define __FORMULA_HPP__
 
-class Formula {
+#include <vector>
+#include <memory>
+#include <string>
+#include <set>
+
+class BaseFormula;
+using Formula = std::shared_ptr<BaseFormula>;
+
+class BaseFormula {
 	public:
-		virtual ~Formula()     {	}
-		virtual void printFormula() const = 0;
+		virtual ~BaseFormula()     {	}
+		virtual void PrintFormula() const = 0;
+		virtual std::string GetType() const = 0;
+		virtual Formula ToNNF() const= 0;
+		virtual Formula GetFormulaData() const {	}
+		virtual Formula GetFormulaDataL() const {	}
+		virtual Formula GetFormulaDataR() const {	}
 };
 
 
-class Atom : public Formula {
+
+class True: public BaseFormula {
 	public:
-		Atom(unsigned _n) : n(_n) {		}
-		void printFormula() const;
+		True() {	}
+		void PrintFormula() const;
+		std::string GetType() const;
+		Formula ToNNF() const;
+};
+
+
+class False : public BaseFormula {
+	public: 
+		False() {	}
+		void PrintFormula() const;
+		std::string GetType() const;
+		Formula ToNNF() const;
+};
+
+
+class Atom : public BaseFormula {
+	public:
+		Atom(int _n) : n(_n) {		}
+		void PrintFormula() const;
+		std::string GetType() const;
+		Formula ToNNF() const;
 	private:
-		unsigned n;
+		int n;
 };
 
 
-class Not : public Formula {
+class Not : public BaseFormula {
 	public:
-		Not(Formula *_f) : f(_f) 	{	}
-		~Not();
-		void printFormula() const;
+		Not(Formula _f) : f(_f) 	{	}
+		void PrintFormula() const;
+		std::string GetType() const;
+		Formula GetFormulaData() const;
+		Formula ToNNF() const;
 	private:
-		Formula *f;
+		Formula f;
 };
 
 
-class Binary: public Formula {
+class Binary: public BaseFormula {
 	public:
 		virtual ~Binary() {		}
 };
@@ -35,49 +71,57 @@ class Binary: public Formula {
 
 class And : public Binary {
 	public:
-		And(Formula *_l, Formula *_r) : l(_l), r(_r)   {	}
-		~And();
-		void printFormula() const;
-	
+		And(Formula _l, Formula _r) : l(_l), r(_r)   {	}
+		void PrintFormula() const;
+		std::string GetType() const;
+		Formula GetFormulaDataL() const;
+		Formula GetFormulaDataR() const;
+		Formula ToNNF() const;
 	private:
-		Formula *l;
-		Formula *r;
+		Formula l;
+		Formula r;
 }; 
 
 
 class Or : public Binary {
 	public:
-		Or(Formula *_l, Formula *_r) : l(_l), r(_r)   {	}
-		~Or();
-		void printFormula() const;
-	
+		Or(Formula _l, Formula _r) : l(_l), r(_r)   {	}
+		void PrintFormula() const;
+		std::string GetType() const;
+		Formula GetFormulaDataL() const;
+		Formula GetFormulaDataR() const;
+		Formula ToNNF() const;
 	private:
-		Formula *l;
-		Formula *r;
+		Formula l;
+		Formula r;
 };
 
 
 class Impl : public Binary {
 	public:
-		Impl(Formula *_l, Formula *_r) : l(_l), r(_r)   {	}
-		~Impl();
-		void printFormula() const;
-	
+		Impl(Formula _l, Formula _r) : l(_l), r(_r)   {	}
+		void PrintFormula() const;
+		std::string GetType() const;
+		Formula GetFormulaDataL() const;
+		Formula GetFormulaDataR() const;
+		Formula ToNNF() const;
 	private:
-		Formula *l;
-		Formula *r;
+		Formula l;
+		Formula r;
 };
 
 
 class Eql : public Binary {
 	public:
-		Eql(Formula *_l, Formula *_r) : l(_l), r(_r)   {	}
-		~Eql();
-		void printFormula() const;
-	
+		Eql(Formula _l, Formula _r) : l(_l), r(_r)   {	}
+		void PrintFormula() const;
+		std::string GetType() const;
+		Formula GetFormulaDataL() const;
+		Formula GetFormulaDataR() const;
+		Formula ToNNF() const;
 	private:
-		Formula *l;
-		Formula *r;
+		Formula l;
+		Formula r;
 };
 
 
